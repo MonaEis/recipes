5071b4741c7745251e733c1bb48e16793435268b
MonaEis <mona.eismann@gmx.de>
2025-05-15 16:20:04 +0200

    meine rezepte finished

--- src/pages/MeineRezeptePage.tsx
+++ src/pages/MeineRezeptePage.tsx
@@ -1,10 +1,62 @@
-
+import { useQuery } from "@tanstack/react-query";
+import { NavLink } from "react-router";
+import { supabase } from "../lib/supabaseClient";
+import { useAuthContext } from "../context/auth-context";
+import "../styles/meine_rezepte_page.css";
 const MeineRezeptePage = () => {
-  return (
-    <div>
-      Meine Rezepte
-    </div>
-  )
-}
+    const { session } = useAuthContext();
+    
+    const { data, isError, isPending } = useQuery({
+        queryKey: ["recipe_favorites"],
+        queryFn: async () => {
+            const result = await supabase
+                .from("recipe_favorites")
+                .select("*, recipes(*)")
+                .eq("user_id", session!.user.id);
+                console.log(result.data)
+            if (result.error) {
+                throw result.error;
+            }
+            if (result.data) {
+                return result.data;
+            }
+        },
+    });
+    if (isPending) {
+        return "Loading Messages...";
+    }
 
-export default MeineRezeptePage
+    if (isError) {
+        return "Error";
+    }
+
+    
+    return (
+        <article className="recipe_favorites">
+            <h2>Meine Rezept Favoriten</h2>
+            <section className="favorites">
+                {data &&
+                    data.map((favorite) => (
+                        <div key={favorite.recipe_id} className="tile">
+                            <img
+                                src={favorite.recipes.imageUrl?.toString()}
+                                alt={favorite.recipes.name}
+                            />
+                            <div className="tile_text">
+                                <h4>{favorite.recipes.name}</h4>
+                                <p>{favorite.recipes.description}</p>
+                                <NavLink
+                                    className="green_btn"
+                                    to={`/rezepte/${favorite.recipe_id}`}
+                                >
+                                    Zum Rezept
+                                </NavLink>
+                            </div>
+                        </div>
+                    ))}
+            </section>
+        </article>
+    );
+};
+
+export default MeineRezeptePage;
