930f6968dc600b69b4bbbb846085d2a2ecf4d492
MonaEis <mona.eismann@gmx.de>
2025-06-15 21:54:03 +0200

    MediaQueries Navi + Header

--- src/components/Navi.tsx
+++ src/components/Navi.tsx
@@ -4,6 +4,7 @@ import logoImg from "../assets/img/loeffelgruen_logo.svg";
 import LoginStatus from "./LoginStatus";
 import { useAuthContext } from "../context/auth-context";
 import profilIcon from "../assets/img/profil_icon.png";
+
 const Navi = () => {
     const { session } = useAuthContext();
     return (
@@ -28,14 +29,21 @@ const Navi = () => {
                     <NavLink className="active_link" to={"/ueberuns"}>
                         Über uns
                     </NavLink>
-                    {session && (<> 
-                        <NavLink className="active_link" to={"/neuesrezept"}>
-                            Neues Rezept
-                        </NavLink>
-                        <NavLink className="active_link" to={"/meine_rezepte"}>
-                            Meine Rezepte
-                        </NavLink>
-                    </>
+                    {session && (
+                        <>
+                            <NavLink
+                                className="active_link"
+                                to={"/neuesrezept"}
+                            >
+                                Neues Rezept
+                            </NavLink>
+                            <NavLink
+                                className="active_link"
+                                to={"/meine_rezepte"}
+                            >
+                                Meine Rezepte
+                            </NavLink>
+                        </>
                     )}
                 </div>
                 <div className="rechts">
@@ -47,6 +55,57 @@ const Navi = () => {
                         </Link>
                     )}
                     <LoginStatus />
+                </div>
+            </div>
+
+            <div className="navi_mobile">
+                <div className="upper">
+                    <a href="/">
+                        <div className="logo">
+                            <img src={logoImg} alt="Logo" />
+                            <h2>Löffelgrün</h2>
+                        </div>
+                    </a>
+                    <div className="rechts">
+                        {session && (
+                            <Link to="/profil">
+                                <div className="profil_icon">
+                                    <img src={profilIcon} alt="Profil Icon" />
+                                </div>
+                            </Link>
+                        )}
+                        <LoginStatus />
+                    </div>
+                </div>
+                <div className="small_color_stripe"></div>
+                <div className="links">
+                    <NavLink className="active_link" to={"/"}>
+                        Home
+                    </NavLink>
+                    {session && (
+                        <NavLink className="active_link" to={"/rezepte"}>
+                            Rezepte
+                        </NavLink>
+                    )}
+                    <NavLink className="active_link" to={"/ueberuns"}>
+                        Über uns
+                    </NavLink>
+                    {session && (
+                        <>
+                            <NavLink
+                                className="active_link"
+                                to={"/neuesrezept"}
+                            >
+                                Neues Rezept
+                            </NavLink>
+                            <NavLink
+                                className="active_link"
+                                to={"/meine_rezepte"}
+                            >
+                                Meine Rezepte
+                            </NavLink>
+                        </>
+                    )}
                 </div>
             </div>
         </nav>
